6fc070aabd3450365aba19a6c4cd73c969c0d5d9
suarezKevin <suarezk453@gmail.com>
2022-09-25 16:44:48 -0500

    Crear el proyecto base de control de inventario backend

---
+++ Inventory/src/test/java/com/suakez/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suakez.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
